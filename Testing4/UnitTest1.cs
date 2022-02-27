4dee30ea4782b4b6aace1a01c197d6c7bfce8879
connor burton <connorsteven2002@hotmail.com>
2022-02-27 18:16:55 +0000

    changes to unit test and clssuplier

--- Testing4/UnitTest1.cs
+++ Testing4/UnitTest1.cs
@@ -14,5 +14,62 @@ namespace Testing4
             //test to see if it is a supplier 
             Assert.IsNotNull(ASupplier);
         }
+        [TestMethod]
+        public void TestMethod2 ()
+        {
+            clssupplier ASupplier = new clssupplier { };
+            Boolean testData = true;
+            ASupplier.Active = testData;
+            Assert.AreEqual(ASupplier.Active, testData);
+        }
+        [TestMethod]
+        public void testdate()
+        {
+            clssupplier ASupplier = new clssupplier { };
+            DateTime TestData = DateTime.Now.Date;
+            ASupplier.DateAdded = TestData;
+            Assert.AreEqual(ASupplier.DateAdded, TestData);
+        }
+        [TestMethod]
+        public void testSup_Name()
+        {
+            clssupplier ASupplier = new clssupplier { };
+            string testData = "company";
+            ASupplier.sup_Name = "company";
+            Assert.AreEqual(ASupplier.sup_Name, testData);
+        }
+        [TestMethod]
+        public void test_Sup_Start_Date()
+        {
+            clssupplier Asupplier = new clssupplier { };
+            DateTime testData = DateTime.Now.Date;
+            Asupplier.Sup_Start_Date = testData;
+            Assert.AreEqual(Asupplier.Sup_Start_Date, testData);
+
+        }
+        [TestMethod]
+        public void test_available()
+        {
+            clssupplier Asupplier = new clssupplier { };
+            int testData = 0;
+            Asupplier.available = testData;
+            Assert.AreEqual(Asupplier.available, testData);
+        }
+        [TestMethod]
+        public void test_phones_sup()
+        {
+            clssupplier Asup = new clssupplier { };
+            string testdata = "a52";
+            Asup.phones = testdata;
+            Assert.AreEqual(Asup.phones, testdata);
+        }
+        [TestMethod] 
+        public void test_contact ()
+        {
+            clssupplier asup = new clssupplier { };
+            string testdata = "012";
+            asup.contact = testdata;
+            Assert.AreEqual(asup.contact, testdata);
+        }
     }
 }
--- Testing4/clssupplier.cs
+++ Testing4/clssupplier.cs
@@ -1,6 +1,15 @@
-﻿namespace Testing4
+﻿using System;
+
+namespace Testing4
 {
     public class clssupplier
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string sup_Name { get; set; }
+        public DateTime Sup_Start_Date { get; set; }
+        public int available { get; internal set; }
+        public string phones { get; internal set; }
+        public string contact { get; internal set; }
     }
 }
