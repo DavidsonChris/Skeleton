c63e3fcc6c681144dd7292b132310d9a8d381183
connor burton <connorsteven2002@hotmail.com>
2022-03-30 17:54:46 +0100

    creating find method

--- ClassLibrary/clssupplier.cs
+++ ClassLibrary/clssupplier.cs
@@ -4,12 +4,34 @@ namespace ClassLibrary
 {
     public class clssupplier
     {
+        private string msup_Name;
+        private DateTime mDateAdded;
+        private DateTime mSup_Start_Date;
+        private int mavailable;
+        private string mphones;
+        private string mcontact;
         public bool Active { get; set; }
-        public DateTime DateAdded { get; set; }
-        public string sup_Name { get; set; }
-        public DateTime Sup_Start_Date { get; set; }
-        public int available { get; set; }
-        public string phones { get; set; }
-        public string contact { get; set; }
+        public DateTime DateAdded { get { return mDateAdded; } set { mDateAdded = value; } }
+        public string sup_Name
+        {
+            get
+            { return msup_Name;}
+            set 
+            { msup_Name = value;}
+        }
+        public DateTime Sup_Start_Date { get { return mSup_Start_Date; } set { mSup_Start_Date = value; } }
+        public int available { get { return mavailable; } set { mavailable = value; } }
+        public string phones { get { return mphones; } set { mphones = value; } }
+        public string contact { get { return mcontact; } set { mcontact = value; } }
+
+        public bool find(string supplier_Name)
+        {
+            msup_Name = "asdf";
+            mSup_Start_Date = Convert.ToDateTime("01/01/2018");
+            mavailable = 1;
+            mphones = "a52";
+            mcontact = "example@gmail.com";
+            return true;
+        }
     }
 }
--- Testing4/UnitTest1.cs
+++ Testing4/UnitTest1.cs
@@ -72,5 +72,86 @@ namespace Testing4
             asup.contact = testdata;
             Assert.AreEqual(asup.contact, testdata);
         }
+        [TestMethod]
+        public void findMethodOK()
+        {
+            clssupplier asup = new clssupplier();
+            Boolean Found = false;
+            string supplier_Name = "asdf";
+            Found = asup.find(supplier_Name);
+            Assert.IsTrue(Found);
+
+        }
+        [TestMethod]
+        public void TestSup_Name()
+        {
+            clssupplier asup = new clssupplier();
+            Boolean Found =false;
+            Boolean OK = true;
+            string supplier_Name = "asdf";
+            Found = asup.find(supplier_Name);
+            if (asup.sup_Name != "asdf")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestSup_Start_Date()
+        {
+            clssupplier asup = new clssupplier();
+            Boolean Found = false;
+            Boolean OK = true;
+            string supplier_Name = "asdf";
+            Found = asup.find(supplier_Name);
+            if (asup.Sup_Start_Date != Convert.ToDateTime("01/01/2018"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestAvailabilty()
+        {
+            clssupplier asup = new clssupplier();
+            Boolean Found = false;
+            Boolean OK = true;
+            string supplier_Name = "asdf";
+            Found = asup.find(supplier_Name);
+            if (asup.available != 1)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void Testphones()
+        {
+            clssupplier asup = new clssupplier();
+            Boolean Found = false;
+            Boolean OK = true;
+            string supplier_Name = "asdf";
+            Found = asup.find(supplier_Name);
+            if (asup.phones != "a52")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void Testcontact()
+        {
+            clssupplier asup = new clssupplier();
+            Boolean Found = false;
+            Boolean OK = true;
+            string supplier_Name = "asdf";
+            Found = asup.find(supplier_Name);
+            if (asup.contact != "example@gmail.com")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
     }
 }
